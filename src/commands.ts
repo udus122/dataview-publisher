e79184030f181592f8f8cb32d93065b1e4cb56c2
yudai.usuda <yudai.usuda@zozo.com>
2024-06-10 13:37:24 +0900

    feat: add insert template command

--- src/commands.ts
+++ src/commands.ts
@@ -1,6 +1,5 @@
-import { type Command } from "obsidian";
+import { Notice, type Command } from "obsidian";
 import { Settings } from "./settings";
-// import { serializeDataview } from "./operations";
 
 import type { UnsafeApp } from "./types";
 import { Operator } from "./operations";
@@ -8,7 +7,7 @@ import { Operator } from "./operations";
 export function createCommands(app: UnsafeApp, settings: Settings): Command[] {
   return [
     {
-      id: "update-dataview-blocks",
+      id: "dataview-publisher-update-blocks",
       name: "Update dataview blocks",
       callback: () => {
         const operator = new Operator(app);
@@ -16,13 +15,39 @@ export function createCommands(app: UnsafeApp, settings: Settings): Command[] {
       },
     },
     {
-      id: "update-dataview-blocks-and-publish",
+      id: "dataview-publisher-update-blocks-and-publish",
       name: "Update dataview blocks and publish",
       callback: () => {
         const operator = new Operator(app);
         operator.updateFromSource(settings.source);
         // Open Obsidian Publish
         app.commands.executeCommandById("publish:view-changes");
+      },
+    },
+    {
+      id: "dataview-publisher-insert-block",
+      name: "Insert dataview publish block",
+      editorCallback: (editor) => {
+        const { line, ch } = editor.getCursor();
+        const lineContent = editor.getLine(line);
+
+        try {
+          editor.replaceRange(
+            `
+%% DATAVIEW_PUBLISHER: start
+\`\`\`dataview
+\`\`\`
+%%
+%% DATAVIEW_PUBLISHER: end %%`,
+            {
+              line,
+              ch: lineContent.length,
+            }
+          );
+          editor.setCursor(line, ch);
+        } catch (e) {
+          new Notice(e.message);
+        }
       },
     },
   ];
