af79a39e18246a5954c347459fd65b3782a630a2
yudai.usuda <yudai.usuda@zozo.com>
2024-06-10 18:54:43 +0900

    fix: Fixed an issue where the default save behavior would not work.

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import type { UnsafeApp } from "./types";
 
 export default class Main extends Plugin {
   settings: Settings;
+  private originalSaveCallback: () => void;
 
   async onload() {
     await this.loadSettings();
@@ -21,22 +22,39 @@ export default class Main extends Plugin {
     // Source for save setting
     // https://github.com/hipstersmoothie/obsidian-plugin-prettier/blob/main/src/main.ts
     // NOTE: To avoid type errors, we need to cast the command definition to the correct type
-    const app = this.app as UnsafeApp;
-    const saveCommandDefinition = app.commands.commands["editor:save-file"] as {
-      callback: () => void | undefined;
-    };
+    const saveCommandDefinition = this.getSaveCommandDefinition();
+    this.originalSaveCallback = saveCommandDefinition.callback;
 
     if (typeof saveCommandDefinition.callback === "function") {
       saveCommandDefinition.callback = () => {
+        this.originalSaveCallback();
+
         if (this.settings.updateOnSave) {
-          const operator = new Operator(app);
+          const operator = new Operator(this.app as UnsafeApp);
           operator.updateActiveFile();
         }
       };
     }
   }
 
-  onunload() {}
+  onunload() {
+    const saveCommandDefinition = this.getSaveCommandDefinition();
+    if (
+      saveCommandDefinition &&
+      saveCommandDefinition.callback &&
+      this.originalSaveCallback
+    ) {
+      saveCommandDefinition.callback = this.originalSaveCallback;
+    }
+  }
+
+  private getSaveCommandDefinition() {
+    const app = this.app as UnsafeApp;
+    const saveCommandDefinition = app.commands.commands["editor:save-file"] as {
+      callback: () => void | undefined;
+    };
+    return saveCommandDefinition;
+  }
 
   async loadSettings() {
     this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
