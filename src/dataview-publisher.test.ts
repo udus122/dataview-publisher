ab11b7ec1110c53c2cb11078b55b0fbaa7bd96e9
yudai.usuda <yudai.usuda@zozo.com>
2024-06-10 13:37:01 +0900

    feat: change mark

--- src/dataview-publisher.test.ts
+++ src/dataview-publisher.test.ts
@@ -7,46 +7,46 @@ import {
 const TEST_TEXT = `
 Necessitatibus quisquam veritatis eos dolor hic totam sapiente necessitatibus est. Eaque maxime nisi velit fugiat sint. Non natus nam illo. Dolorum earum esse quod vitae autem.
 
-%% DATAVIEW_PUBLISH: start
+%% DATAVIEW_PUBLISHER: start
 \`\`\`dataview
 LIST
 FROM #🏷️/dataview-publish 
 \`\`\`
 %%
 DATAVIEWの結果をシリアライズした結果であり、置換対象
-%% DATAVIEW_PUBLISH: end %%
+%% DATAVIEW_PUBLISHER: end %%
 
 Necessitatibus quisquam veritatis eos dolor hic totam sapiente necessitatibus est. Eaque maxime nisi velit fugiat sint. Non natus nam illo. Dolorum earum esse quod vitae autem.
 
 Necessitatibus quisquam veritatis eos dolor hic totam sapiente necessitatibus est. Eaque maxime nisi velit fugiat sint. Non natus nam illo. Dolorum earum esse quod vitae autem.
 
-%% DATAVIEW_PUBLISH: start
+%% DATAVIEW_PUBLISHER: start
 \`\`\`dataview
 LIST
 FROM #🏷️/index
 \`\`\`
 %%
 DATAVIEWの結果をシリアライズした結果であり、置換対象
-%% DATAVIEW_PUBLISH: end %%
+%% DATAVIEW_PUBLISHER: end %%
 
 Necessitatibus quisquam veritatis eos dolor hic totam sapiente necessitatibus est. Eaque maxime nisi velit fugiat sint. Non natus nam illo. Dolorum earum esse quod vitae autem.
 `;
 
-const TEST_BLOCK = `%% DATAVIEW_PUBLISH: start
+const TEST_BLOCK = `%% DATAVIEW_PUBLISHER: start
 \`\`\`dataview
 LIST
 FROM #🏷️/dataview-publish 
 \`\`\`
 %%
 DATAVIEWの結果をシリアライズした結果であり、置換対象
-%% DATAVIEW_PUBLISH: end %%`;
+%% DATAVIEW_PUBLISHER: end %%`;
 
 describe("operations", () => {
   it("parseBlock", () => {
     const parsed = parseBlock(TEST_BLOCK);
     expect(parsed).toEqual({
       content: TEST_BLOCK,
-      startBlock: `%% DATAVIEW_PUBLISH: start
+      startBlock: `%% DATAVIEW_PUBLISHER: start
 \`\`\`dataview
 LIST
 FROM #🏷️/dataview-publish 
@@ -55,7 +55,7 @@ FROM #🏷️/dataview-publish
       query: "LIST\nFROM #🏷️/dataview-publish",
       language: "dataview",
       serialized: `DATAVIEWの結果をシリアライズした結果であり、置換対象`,
-      endBlock: "%% DATAVIEW_PUBLISH: end %%",
+      endBlock: "%% DATAVIEW_PUBLISHER: end %%",
     });
   });
 
@@ -63,22 +63,22 @@ FROM #🏷️/dataview-publish
     const blocks = extractBlock(TEST_TEXT);
     expect(blocks.length).toBe(2);
     expect(blocks).toEqual([
-      `%% DATAVIEW_PUBLISH: start
+      `%% DATAVIEW_PUBLISHER: start
 \`\`\`dataview
 LIST
 FROM #🏷️/dataview-publish 
 \`\`\`
 %%
 DATAVIEWの結果をシリアライズした結果であり、置換対象
-%% DATAVIEW_PUBLISH: end %%`,
-      `%% DATAVIEW_PUBLISH: start
+%% DATAVIEW_PUBLISHER: end %%`,
+      `%% DATAVIEW_PUBLISHER: start
 \`\`\`dataview
 LIST
 FROM #🏷️/index
 \`\`\`
 %%
 DATAVIEWの結果をシリアライズした結果であり、置換対象
-%% DATAVIEW_PUBLISH: end %%`,
+%% DATAVIEW_PUBLISHER: end %%`,
     ]);
   });
 
@@ -87,69 +87,69 @@ DATAVIEWの結果をシリアライズした結果であり、置換対象
     const parsed = blocks.map((block) => parseBlock(block));
     expect(parsed).toEqual([
       {
-        content: `%% DATAVIEW_PUBLISH: start
+        content: `%% DATAVIEW_PUBLISHER: start
 \`\`\`dataview
 LIST
 FROM #🏷️/dataview-publish 
 \`\`\`
 %%
 DATAVIEWの結果をシリアライズした結果であり、置換対象
-%% DATAVIEW_PUBLISH: end %%`,
+%% DATAVIEW_PUBLISHER: end %%`,
         language: "dataview",
         query: "LIST\nFROM #🏷️/dataview-publish",
         serialized: `DATAVIEWの結果をシリアライズした結果であり、置換対象`,
-        startBlock: `%% DATAVIEW_PUBLISH: start
+        startBlock: `%% DATAVIEW_PUBLISHER: start
 \`\`\`dataview
 LIST
 FROM #🏷️/dataview-publish 
 \`\`\`
 %%`,
-        endBlock: "%% DATAVIEW_PUBLISH: end %%",
+        endBlock: "%% DATAVIEW_PUBLISHER: end %%",
       },
       {
-        content: `%% DATAVIEW_PUBLISH: start
+        content: `%% DATAVIEW_PUBLISHER: start
 \`\`\`dataview
 LIST
 FROM #🏷️/index
 \`\`\`
 %%
 DATAVIEWの結果をシリアライズした結果であり、置換対象
-%% DATAVIEW_PUBLISH: end %%`,
+%% DATAVIEW_PUBLISHER: end %%`,
         language: "dataview",
         query: "LIST\nFROM #🏷️/index",
         serialized: `DATAVIEWの結果をシリアライズした結果であり、置換対象`,
-        startBlock: `%% DATAVIEW_PUBLISH: start
+        startBlock: `%% DATAVIEW_PUBLISHER: start
 \`\`\`dataview
 LIST
 FROM #🏷️/index
 \`\`\`
 %%`,
-        endBlock: "%% DATAVIEW_PUBLISH: end %%",
+        endBlock: "%% DATAVIEW_PUBLISHER: end %%",
       },
     ]);
   });
   it("should return the composed block", () => {
     const blocks = {
       content:
-        "%% DATAVIEW_PUBLISH: start\n```dataview\nLIST\nFROM #🏷️/dataview-publish \n```\n%%\nDATAVIEWの結果をシリアライズした結果であり、置換対象\n%% DATAVIEW_PUBLISH: end %%",
+        "%% DATAVIEW_PUBLISHER: start\n```dataview\nLIST\nFROM #🏷️/dataview-publish \n```\n%%\nDATAVIEWの結果をシリアライズした結果であり、置換対象\n%% DATAVIEW_PUBLISHER: end %%",
       startBlock:
-        "%% DATAVIEW_PUBLISH: start\n```dataview\nLIST\nFROM #🏷️/dataview-publish \n```\n%%",
+        "%% DATAVIEW_PUBLISHER: start\n```dataview\nLIST\nFROM #🏷️/dataview-publish \n```\n%%",
       query: "LIST\nFROM #🏷️/dataview-publish",
       language: "dataview",
       serialized: "DATAVIEWの結果をシリアライズした結果であり、置換対象",
-      endBlock: "%% DATAVIEW_PUBLISH: end %%",
+      endBlock: "%% DATAVIEW_PUBLISHER: end %%",
     };
 
     const composedBlock = composeBlockContent(blocks);
 
-    const expectedBlock = `%% DATAVIEW_PUBLISH: start
+    const expectedBlock = `%% DATAVIEW_PUBLISHER: start
 \`\`\`dataview
 LIST
 FROM #🏷️/dataview-publish 
 \`\`\`
 %%
 DATAVIEWの結果をシリアライズした結果であり、置換対象
-%% DATAVIEW_PUBLISH: end %%`;
+%% DATAVIEW_PUBLISHER: end %%`;
 
     expect(composedBlock).toEqual(expectedBlock);
   });
